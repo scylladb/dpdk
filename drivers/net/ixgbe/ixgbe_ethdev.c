d42481c44c2f4b555794bf15c47b1e0add9a1d8c
Vlad Zolotarov <vladz@cloudius-systems.com>
2019-06-10 13:41:07 +0800

    ixgbe_pmd: forbid tx_rs_thresh above 1 for all NICs but 82598
    According to 82599 and x540 HW specifications RS bit *must* be
    set in the last descriptor of *every* packet.
    
    This patch fixes the Tx hang we were constantly hitting with a
    seastar-based application on x540 NIC.
    
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>
    Signed-off-by: Takuya ASADA <syuu@scylladb.com>
    Signed-off-by: Yibo Cai <yibo.cai@arm.com>
    Message-Id: <20190610054109.22628-1-yibo.cai@arm.com>

--- drivers/net/ixgbe/ixgbe_ethdev.c
+++ drivers/net/ixgbe/ixgbe_ethdev.c
@@ -3782,6 +3782,14 @@ ixgbe_dev_info_get(struct rte_eth_dev *dev, struct rte_eth_dev_info *dev_info)
 		.offloads = 0,
 	};
 
+	/*
+	 * According to 82599 and x540 specifications RS bit *must* be set on the
+	 * last descriptor of *every* packet. Therefore we will not allow the
+	 * tx_rs_thresh above 1 for all NICs newer than 82598.
+	 */
+	if (hw->mac.type > ixgbe_mac_82598EB)
+		dev_info->default_txconf.tx_rs_thresh = 1;
+
 	dev_info->rx_desc_lim = rx_desc_lim;
 	dev_info->tx_desc_lim = tx_desc_lim;
 
